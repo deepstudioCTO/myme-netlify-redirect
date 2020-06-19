a2b35ae342ab6fc171c97891b1c92bd4c40829cf
cheetah <cto@deepstudio.io>
2020-06-19 18:07:05 +0900

    redirect to myme.deepstudio.io

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    window.location.href = 'https://myme.deepstudio.io/';
+  }, [])
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
